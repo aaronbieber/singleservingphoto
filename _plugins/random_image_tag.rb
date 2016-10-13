e3023aff9dfd25f02f28f7746bcc7fdd107c4f89
Aaron Bieber <aaron@aaronbieber.com>
2016-10-13 06:23:12 -0400

    Add random image Liquid tag plugin

---
+++ _plugins/random_image_tag.rb
@@ -0,0 +1,26 @@
+# Random image tag for Jekyll
+
+module Jekyll
+  class RandomImageTag < Liquid::Tag
+    @image = ''
+    @direction = 'left'
+
+    def initialize(tag_name, markup, tokens)
+      if markup =~ /(?<direction>left|right)/i
+        @direction = $~['direction'].strip
+      end
+
+      images = Dir["ssp/*.jpg"]
+      @image = images[rand(images.length)]
+
+      super
+    end
+
+    def render(context)
+      "![](/#{@image}){:.random-#{@direction}}"
+    end
+
+  end
+end
+
+Liquid::Template.register_tag('random', Jekyll::RandomImageTag)
